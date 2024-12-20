9fee2d72beb5ab810ce0c13a3c6eef26058c9979
JLo521 <jack.losing@ndsu.edu>
2024-12-20 13:46:20 -0600

    fized probs

--- userdetails.cs
+++ userdetails.cs
@@ -2,7 +2,7 @@
 {
     public class userDetails
     {
-        public static int id { get; set; }
-        public static bool isAdmin{ get; set; }
+        public static int id = -1;
+        public static bool isAdmin = false;
     }
 }
